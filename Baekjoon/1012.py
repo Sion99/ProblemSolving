6fbf11fd25a5222729bf500da034f185eed6920b
sion99 <kmicety1@gmail.com>
2022-11-21 20:21:17 +0900

    1764.py

--- Baekjoon/1012.py
+++ Baekjoon/1012.py
@@ -1,8 +1,35 @@
-#S2 유기농 배추
+# S2 유기농 배추
+import sys
+sys.setrecursionlimit(10000)
+
+
+def dfs(x, y):
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+
+    for i in range(4):
+        next_x = x + dx[i]
+        next_y = y + dy[i]
+        if (0 <= next_x < m) and (0 <= next_y < n):
+            if matrix[next_y][next_x] == 1:
+                matrix[next_y][next_x] = -1
+                dfs(next_x, next_y)
+
 
 t = int(input())
 for i in range(t):
-    n, m, k = map(int, input().split())
+    m, n, k = map(int, input().split())
+    matrix = [[0]*m for i in range(n)]
+    count = 0
+
     for j in range(k):
         x, y = map(int, input().split())
-        
+        matrix[y][x] = 1
+
+    for ii in range(m):
+        for jj in range(n):
+            if matrix[jj][ii] == 1:
+                dfs(ii, jj)
+                count += 1
+
+    print(count)

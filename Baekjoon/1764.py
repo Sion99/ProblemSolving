fca0be0f76c724e35167d09471af96c5020f5de7
sion99 <kmicety1@gmail.com>
2022-11-21 20:49:24 +0900

    1764.py

--- Baekjoon/1764.py
+++ Baekjoon/1764.py
@@ -1,1 +1,13 @@
 # S4 듣보잡
+import sys
+
+n, m = map(int, input().split())
+
+
+haventheard = [sys.stdin.readline().strip() for i in range(n)]
+haventseen = [sys.stdin.readline().strip() for j in range(m)]
+heardseen = list(sorted(set(haventheard) & set(haventseen)))
+
+print(len(heardseen))
+for i in range(len(heardseen)):
+    print(heardseen[i], end='\n')

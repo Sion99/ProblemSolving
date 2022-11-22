ce0eccba2adf5a813291e2d5c46d8eac20289400
sion99 <kmicety1@gmail.com>
2022-11-22 10:36:17 +0900

    5597.c

---
+++ Baekjoon/5597.c
@@ -0,0 +1,24 @@
+// B5 과제 안 내신 분..?
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int arr[31];
+    for (int i = 0; i < 31; i++)
+    {
+        arr[i] = 0;
+    }
+    for (int i = 0; i < 28; i++)
+    {
+        scanf("%d", &n);
+        arr[n] = n;
+    }
+    for (int i = 1; i < 31; i++)
+    {
+        if (arr[i] == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}

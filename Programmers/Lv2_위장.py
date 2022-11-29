fda634888598bf2004bb70380065f57dc25821b1
sion99 <kmicety1@gmail.com>
2022-11-30 01:12:42 +0900

    11/30 programmers

--- Programmers/Lv2_위장.py
+++ Programmers/Lv2_위장.py
@@ -1,74 +1,29 @@
 # 11/29 21:00-
 # 생각보다 많이 어려운데? 22:23 까지 못품..
 
-from itertools import combinations
-
-
-def solution(clothes):
-    answer = 0
-    # 매일 다른 옷을 조합하여 입기
-    # clothes는 옷에 대한 2차원 배열 [이름, 종류]
-    # 서로 다른 옷의 조합의 수를 return
-    # 같은 이름의 의상은 존재하지 않음
-
-    ctype = []
-
-    for i in clothes:
-        ctype.append(i[1])
-
-    sum = 0
-    sum += len(ctype)
-    # sum += len(ctype)-1
-    if len(ctype) <= 1:
-        answer = len(ctype)
-    elif len(ctype) == 2:
-        answer = len(ctype)+1
-    for i in range(2, len(ctype)/2):
-        temp = list(set(list(combinations(ctype, i))))
-        temp2 = len(temp)
-
-        # for j in range(len(temp)):
-        #     if temp.count(temp[j]) != 1:
-        #         temp2 = temp2 - 1
-
-        print(temp)
-        print(temp2)
-        sum += temp2
-
-    answer = sum
-    return answer
-
+# 결국 거의 두 시간 쓰고 다른 사람 아이디어를 통해 풀었다
+# 코드를 본 것은 아니지만 수학적 접근법을 제시해줘서 덕분에 풀 수 있었다.
+# 내가 하려 했던 것은 combinations을 이용해 조합을 구하여 어떻게 어떻게 하려고 했는데,
+# 굉장히 비효율적이고 오류 많은 코드였던 거 같다
 
 def solution(clothes):
     answer = 0
-    # 매일 다른 옷을 조합하여 입기
-    # clothes는 옷에 대한 2차원 배열 [이름, 종류]
-    # 서로 다른 옷의 조합의 수를 return
-    # 같은 이름의 의상은 존재하지 않음
-
-    ctype = []
-
+    clth = []
     for i in clothes:
-        ctype.append(i[1])
+        clth.append(i[1])
 
-    sum = 0
-    # 겹치지 않을 때
-    if len(ctype) == len(set(ctype)):
-        for i in range(1, len(ctype)):
-            temp = list(combinations(ctype, i))
-            sum += len(temp)
+    clth.sort()
+    number = []
+    temp = list(set(clth))
+    for i in temp:
+        number.append(clth.count(i))
 
-    # 겹칠때
-    else:
-        sum += len(ctype)
-        ttype = set(ctype)
-        k = 1
-        while (True):
-            k += 1
-            temp2 = set(list(combinations(ttype, i)))
-            print(temp2)
-            if k >= len(ttype):
-                break
+    func = 1
+    for i in range(len(number)):
+        func = func * (1+number[i])
 
-    answer = sum
+    func = func - 1
+
+    print(func)
+    answer = func
     return answer

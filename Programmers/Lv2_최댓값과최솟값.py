73149dfa3bf50302e65384893bcd8a49e8c0694d
sion99 <kmicety1@gmail.com>
2022-11-30 10:51:08 +0900

    11/30 programmers

---
+++ Programmers/Lv2_최댓값과최솟값.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = ''
+
+    arr = s.split(' ')
+    number = []
+    for i in range(len(arr)):
+        number.append(int(arr[i]))
+
+    number.sort()
+    print(number)
+    answer += str(number[0])
+    answer += ' '
+    answer += str(number[-1])
+
+    return answer

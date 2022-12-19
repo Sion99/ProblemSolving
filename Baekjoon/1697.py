be09b06586c5f8847cc02d3e947c44e93b8c36ae
sion99 <kmicety1@gmail.com>
2022-12-19 21:19:53 +0900

    12/19 programmers

---
+++ Baekjoon/1697.py
@@ -0,0 +1,24 @@
+# 12/18
+# 실버 1 숨바꼭질
+n, k = map(int, input().split())
+# 현재 점 n과 찾고자 하는 점 k
+# 걸을 때는 현재 위치에서 +- 1 만큼, 순간이동을 할 경우에는 현재 위치의 2배
+
+# k-n 이 실제 남은 거리긴 한데..
+count = 0
+# while (k != n):
+#     # count += 1
+#     # if n == 0:
+#     #     n += 1
+#     # else:
+#     #     if k >= 2*n:
+#     #         n = 2*n
+#     #     else:
+#     #         if k
+#     count += 1
+#     if k % 2 == 0:
+#         k = k/2
+#     else:
+
+
+print(count)

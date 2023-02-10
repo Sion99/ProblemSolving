a379bec3d558588e771646ec2455b064876008d1
sion99 <kmicety1@gmail.com>
2023-02-10 16:35:41 +0900

    02/10 baekjoon

--- Baekjoon/1021.py
+++ Baekjoon/1021.py
@@ -2,3 +2,27 @@ from collections import deque
 
 n, m = map(int, input().split())
 
+arr = []
+for i in range(n):
+    arr.append(i + 1)
+
+arr = deque(arr)
+
+count = 0
+list = list(map(int, input().split()))
+for i in list:
+    if arr.index(i) > len(arr) / 2:
+        while True:
+            if arr.index(i) == 0:
+                break
+            arr.rotate(1)
+            count += 1
+    else:
+        while True:
+            if arr.index(i) == 0:
+                break
+            arr.rotate(-1)
+            count += 1
+    arr.popleft()
+
+print(count)

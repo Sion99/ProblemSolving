729d80d7ac36a1dba74f69c9faa06c06fa21fb5a
sion99 <kmicety1@gmail.com>
2023-03-13 17:16:29 +0900

    03/13 baekjoon

---
+++ Baekjoon/15649.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int number[9];
+bool visited[9];
+
+void dfs(int a)
+{
+    if (a == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << number[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited[i] == false)
+        {
+            visited[i] = true;
+            number[a] = i;
+            dfs(a + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main()
+{
+
+    cin >> n >> m;
+    dfs(0);
+
+    return 0;
+}

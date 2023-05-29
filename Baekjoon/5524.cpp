4a8f7376628f0fef1a3ed243cd16930132f7f87a
Sion99 <kmicety1@gmail.com>
2023-05-29 16:50:56 +0900

    🥉 Bronze 4 : 입실 관리

---
+++ Baekjoon/5524.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++)
+		{
+			if (s[j] >= 'A' && s[j] <= 'Z')
+				s[j] += 32;
+		}
+		cout << s << '\n';
+	}
+}

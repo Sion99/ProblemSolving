f4a6ae65483423281a6c62db26231d33aedc3754
Sion99 <kmicety1@gmail.com>
2023-05-30 17:40:14 +0900

    🥈 Silver 3 : N과 M (3)

---
+++ Baekjoon/15651.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int number[8];
+bool visited[8];
+
+void dfs(int a)
+{
+	if (a == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << number[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		number[a] = i;
+		dfs(a + 1);
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	dfs(0);
+}

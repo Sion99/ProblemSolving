86e88521e81b6ead053665940da477551cffb6a7
Sion Shin <kmicety1@gmail.com>
2024-09-09 14:45:51 +0900

    09/09

---
+++ Baekjoon/15656.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+int num[10];
+
+void func(int k) {
+    // k == m에 도달할 경우 다 출력하고 끝
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << num[arr[i]] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    // 아직 채워야 할 공간이 있다면
+    for (int i = 0; i < n; i++) {
+        arr[k] = i;
+        func(k + 1);
+    }
+
+    // 0 -> 0 0 -> 0 0 0
+    // 0 -> 0 1 -> 0 0
+}
+
+int main() {
+
+    cin >> n >> m;
+    // 같은 수 여러번 골라도 됨
+
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    // sort(num, num + n);
+    func(0);
+}
